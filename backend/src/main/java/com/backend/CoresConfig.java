6b2e05117808b3f19ca801ae899748642ad1df07
S_HYEON <zxl3651@naver.com>
2024-09-20 15:57:16 +0900

    backend 헥사고날 아키텍처 구현 - Login 기능

---
+++ backend/src/main/java/com/backend/CoresConfig.java
@@ -0,0 +1,18 @@
+package com.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CoresConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 특정 패턴에 대해 CORS 설정
+                .allowedOrigins("http://localhost:3000")  // React 앱의 URL (포트 포함)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메소드
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .allowCredentials(true);  // 쿠키 허용
+    }
+}
---
+++ backend/src/main/java/com/backend/application/service/login/LoginService.java
@@ -0,0 +1,28 @@
+package com.backend.application.service.login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.backend.adapter.out.repository.login.LoginRepository;
+import com.backend.application.port.in.login.LoginUseCase;
+import com.backend.domain.login.LoginDomain;
+
+@Service
+public class LoginService implements LoginUseCase {
+	
+	@Autowired
+	private LoginRepository repository;
+	
+	// 로그인
+	@Override
+	public int Login(LoginDomain domain) throws Exception {
+		System.out.println("LoginService - Login() called");
+		// 로직 구현
+		domain.pwd_capsule();
+		
+		LoginDomain result = repository.Login(domain);
+		
+		System.out.println("LoginService - Login() complete : " + result + ", return 값: " + result.getLoginResult());
+		return result.getLoginResult();
+	}
+}
