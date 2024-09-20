f0903ebce2935557aead816942549fb8e55f7bc0
KwangMin Kim <Rangbits@gmail.com>
2024-09-20 15:17:00 +0900

    react-i18 사용 다국어처리 구현

--- frontend/src/component/LocaleSelect/index.jsx
+++ frontend/src/component/LocaleSelect/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from 'next/router';
+import { useTranslation } from 'react-i18next';
 import bottomArrow from "./../../../public/images/icons/select-down-arrow-white.svg";
 import koFlag from "./../../../public/images/icons/ic-ko-flag.webp";
 import enFlag from "./../../../public/images/icons/ic-en-flag.webp";
@@ -9,6 +10,7 @@ import jpFlag from "./../../../public/images/icons/ic-jp-flag.webp";
 export default function LocaleSelect() {
     const [selectedLocale, setSelectedLocale] = useState('');
     const [isClicked, setIsClicked] = useState(false);
+    const { i18n } = useTranslation();
     const router = useRouter();
     let localeImg;
 
@@ -18,16 +20,16 @@ export default function LocaleSelect() {
 
     const localeSelected = (locale) => () => {
         setSelectedLocale(locale);
+        i18n.changeLanguage(locale);
         router.push(router.pathname, router.asPath, { locale });
     };
 
     useEffect(() => {
         const storedLocale = localStorage.getItem('selectedLocale');
-        
         if (storedLocale) {
             setSelectedLocale(storedLocale);
         } else {
-            setSelectedLocale('ko');
+            setSelectedLocale('ko_KR');
         }
     }, []);
 
@@ -37,9 +39,9 @@ export default function LocaleSelect() {
         }
     }, [selectedLocale]);
 
-    if(selectedLocale == 'ko') {
+    if(selectedLocale == 'ko_KR') {
         localeImg = koFlag;
-    } else if (selectedLocale == 'en') {
+    } else if (selectedLocale == 'en_US') {
         localeImg = enFlag;
     } else {
         localeImg = jpFlag;
@@ -50,17 +52,17 @@ export default function LocaleSelect() {
             <Image className="rounded-[50%] my-[13.5px] border border-[#505050]" src={localeImg} width={18} height={18} alt="locale"/>
             {isClicked ? 
                 <div className="absolute w-[100px] h-[120px] rounded-[12px] right-2 top-[52px] bg-white z-50">
-                    <div className="flex items-center w-full h-[40px] p-[10px]" onClick={localeSelected("ko")}>
+                    <div className="flex items-center w-full h-[40px] p-[10px]" onClick={localeSelected("ko_KR")}>
                         <Image className="rounded-[50%] h-[18px] border border-[#505050]" src={koFlag} width={18} height={18} alt="ko"/>
-                        <p className="pl-[10px] text-[#505050] font-medium">한국어</p>
+                        <p className="pl-[10px] text-[#505050] text-sm font-medium">한국어</p>
                     </div>
-                    <div className="flex items-center w-full h-[40px] p-[10px] border-y border-[#ebebeb]" onClick={localeSelected("en")}>
-                        <Image className="rounded-[50%] h-[18px] border border-[#505050]" src={enFlag} width={18} height={18} alt="ko"/>
-                        <p className="pl-[10px] text-[#505050] font-medium">English</p>
+                    <div className="flex items-center w-full h-[40px] p-[10px] border-y border-[#ebebeb]" onClick={localeSelected("en_US")}>
+                        <Image className="rounded-[50%] h-[18px] border border-[#505050]" src={enFlag} width={18} height={18} alt="en"/>
+                        <p className="pl-[10px] text-[#505050] text-sm font-medium">English</p>
                     </div>
-                    <div className="flex items-center w-full h-[40px] p-[10px]" onClick={localeSelected("jp")}>
-                        <Image className="rounded-[50%] h-[18px] border border-[#505050]" src={jpFlag} width={18} height={18} alt="ko"/>
-                        <p className="pl-[10px] text-[#505050] font-medium">日本語</p>
+                    <div className="flex items-center w-full h-[40px] p-[10px]" onClick={localeSelected("ja_JP")}>
+                        <Image className="rounded-[50%] h-[18px] border border-[#505050]" src={jpFlag} width={18} height={18} alt="ja"/>
+                        <p className="pl-[10px] text-[#505050] text-sm font-medium">日本語</p>
                     </div>
                 </div> 
                 : ""}
--- frontend/src/pages/test/index.jsx
+++ frontend/src/pages/test/index.jsx
@@ -1,11 +1,17 @@
 import useFormData from "@/component/hooks/useFormData";
 import React from "react";
+import { useTranslation } from 'react-i18next'; // 다국어 처리를 위해 import 하기
 
 export default function index() {
   useFormData();
+  const { t } = useTranslation(); // import한 useTranslation
+
   return (
     <div className="w-full p-4 h-[100% - 41px]">
-      <div>index2</div>
+      {/* json파일에서 단일 key: value */}
+      <div>{t('test')}</div> 
+      {/* json파일에서 key: { key : value } 형태 */}
+      <div>{t('menu.add')}</div>
       <input id="plz2" />
     </div>
   );
