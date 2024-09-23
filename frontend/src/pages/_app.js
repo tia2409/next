fb9ecbb54d1fa77299892c36c503ff685ea62e32
KwangMin Kim <Rangbits@gmail.com>
2024-09-23 14:24:20 +0900

    디테일바 레이아웃 추가 로고변경

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -7,6 +7,7 @@ import Sidebar from "@/component/Sidebar";
 import Tabs from "@/component/Tabs";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../locales/i18n";
+import Detailbar from "@/component/DetailBar";
 
 // 폰트 설정
 const notoSansKR = Noto_Sans_KR({
@@ -18,46 +19,64 @@ const notoSansKR = Noto_Sans_KR({
 function App({ Component, pageProps }) {
   const router = useRouter();
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isDetailbarOpen, setIsDetailbarOpen] = useState(false);
   const [title, setTitle] = useState("");
   const isLoginPage = router.pathname === "/"; // 로그인 페이지 여부 확인
 
   const toggleSidebar = () => {
     setIsSidebarOpen((prev) => !prev);
   };
+  
+  const toggleDetailbar = () => {
+    setIsDetailbarOpen((prev) => !prev);
+  };
+
   useEffect(() => {
     const currentPath = router.pathname;
+    const storedLocale = localStorage.getItem('selectedLocale');
     console.log(currentPath);
     if (currentPath !== "/") {
       setTitle(document.querySelector(`a[href='${currentPath}']`).innerText);
+    }
+    // 로컬에 언어선택 정보가 있는지 확인
+    if (storedLocale) {
+        i18n.changeLanguage(storedLocale);
     }
   }, []);
 
   return (
     <I18nextProvider i18n={i18n}>
-      {isLoginPage ? (
-        <Component {...pageProps} />
-      ) : (
-        <div className={`notoSansKR.className`}>
-          <Header />
-          <div className="relative flex">
-            <Sidebar
-              toggleSidebar={toggleSidebar}
-              isSidebarOpen={isSidebarOpen}
-            />
-            <div className="w-full h-[100vh - 72px]">
-              <Tabs />
-              <div className="w-[calc(100%-40px)] m-[20px] h-[calc(100vh-133px)] bg-[#2d2d2d] p-[20px] mt-0">
-                <div className="main-title">{title}</div>
-                <div className="main-body">
-                  <Component {...pageProps} />
+      <div className={`notoSansKR.className`}>
+        {isLoginPage ? (
+          <Component {...pageProps} />
+        ) : (
+          <>
+            <Header />
+            <div className="relative flex">
+              <Sidebar
+                toggleSidebar={toggleSidebar}
+                isSidebarOpen={isSidebarOpen}
+              />
+              <div className="w-full h-[100vh - 72px]">
+                <Tabs />
+                <div className="w-[calc(100%-40px)] m-[20px] h-[calc(100vh-133px)] bg-[#2d2d2d] p-[20px] mt-0">
+                  <div className="main-title">{title}</div>
+                  <div className="main-body">
+                    <Component {...pageProps} toggleDetailbar={toggleDetailbar} />
+                  </div>
                 </div>
               </div>
+              {isDetailbarOpen && (
+                <Detailbar toggleDetailbar={toggleDetailbar} isDetailbarOpen={isDetailbarOpen} />
+              )}
             </div>
-          </div>
-        </div>
-      )}
+          </>
+        )}
+      </div>
     </I18nextProvider>
   );
+  
+  
 }
 
 export default App;
