bf0ba44a2bdf74d5ea8d17bec805b010523d8c5a
choihyoingwoon <cpobo@hanmail.net>
2024-09-25 15:22:11 +0900

    충돌난거 수정

--- frontend/src/component/CheckInput/index.jsx
+++ frontend/src/component/CheckInput/index.jsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 
 import checkMark from "./../../../public/images/icons/checkbox/checked.svg";
 
-<<<<<<< HEAD
-export default function CheckInput({ inputId, content, ...props }) {
+export default function CheckInput({ inputId, innerText, ...props }) {
   const [isChecked, setIsChecked] = useState(true);
 
   const checkIsChecked = () => {
@@ -13,11 +12,11 @@ export default function CheckInput({ inputId, content, ...props }) {
 
   return (
     <label
-      className="relative p-[12px] pl-[35px] cursor-pointer"
+      className="relative pt-[12px] pl-[35px] cursor-pointer"
       htmlFor={inputId}
     >
-      <span className="flex items-center text-sm font-medium text-[#909090]">
-        {content}
+      <span className="flex items-center text-sm font-semibold text-gray04">
+        {innerText}
       </span>
       <input
         id={inputId}
@@ -27,7 +26,7 @@ export default function CheckInput({ inputId, content, ...props }) {
         {...props}
       />
       <span
-        className="absolute top-[12px] left-0 w-[18px] h-[18px] bg-[#474747]"
+        className="absolute top-[12px] left-0 w-[18px] h-[18px] bg-gray06"
         onClick={checkIsChecked}
       >
         {isChecked ? (
@@ -39,23 +38,3 @@ export default function CheckInput({ inputId, content, ...props }) {
     </label>
   );
 }
-=======
-export default function CheckInput({ inputId, innerText, ...props }) {
-    const [isChecked, setIsChecked] = useState(true);
-  
-    const checkIsChecked = () => {
-      setIsChecked((prev) => !prev);
-    };
-    
-    return (
-      <label className="relative pt-[12px] pl-[35px] cursor-pointer" htmlFor={inputId}>
-          <span className="flex items-center text-sm font-semibold text-gray04">{innerText}</span>
-          <input id={inputId} type="checkbox" checked={isChecked ? "" : "checked"} className="opacity-0"  {...props} />
-          <span className="absolute top-[12px] left-0 w-[18px] h-[18px] bg-gray06" onClick={checkIsChecked}>
-            {isChecked ? <Image src={checkMark} width={18} height={18} alt="check" /> : ""}
-          </span>
-      </label>
-    );
-  }
-  
->>>>>>> 8d472c94fc0b96fe0314ddd2d51c3798aed79840
